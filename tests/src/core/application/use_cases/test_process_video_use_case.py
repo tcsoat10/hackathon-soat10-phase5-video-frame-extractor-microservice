20f4dd81cf8261f77b7f214b8964783ebc7beb67
Carlos Roberto <carlosjr.if@gmail.com>
2025-09-10 00:53:37 -0300

    feat: add tests fails to process video usecase

--- tests/src/core/application/use_cases/test_process_video_use_case.py
+++ tests/src/core/application/use_cases/test_process_video_use_case.py
@@ -1,5 +1,7 @@
 
 from unittest.mock import MagicMock, Mock, patch
+
+import pytest
 from src.core.domain.dtos.video_frame_extractor.process_video_task_dto import ProcessVideoTaskDTO
 from src.core.exceptions.entity_not_found_exception import EntityNotFoundException
 from tests.factories.video_job_factory import VideoJobFactory
@@ -68,3 +70,68 @@ def test_process_video_use_case_video_job_not_found(mock_storage_gateway, mock_f
         assert False, "Expected EntityNotFoundException"
     except EntityNotFoundException as e:
         assert str(e) == "VideoJob com job_ref nonexistent_job_ref não encontrado."
+
+
+@patch('src.infrastructure.repositories.mongoengine.video_job_repository.MongoVideoJobRepository')
+@patch('src.infrastructure.video.ffmpeg_wrapper.FFmpegWrapper')
+@patch('src.infrastructure.gateways.s3_storage_gateway.S3StorageGateway')
+def test_raise_error_when_download_object_fails(mock_storage_gateway, mock_ffmpeg_wrapper, mock_video_job_repository):
+    video_job = VideoJobFactory()
+    mock_video_job_repository.find_by_job_ref.return_value = video_job.to_entity()
+    
+    dto = ProcessVideoTaskDTO(
+        job_ref=video_job.job_ref,
+        client_identification=video_job.client_identification,
+        bucket=video_job.bucket,
+        video_path=video_job.video_path,
+        frames_path=video_job.frames_path,
+        notify_url=video_job.notify_url,
+        config=video_job.config,
+    )
+
+    use_case = ProcessVideoUseCase(
+        video_job_repository=mock_video_job_repository,
+        storage_gateway=mock_storage_gateway,
+        video_processor=mock_ffmpeg_wrapper
+    )
+
+    mock_storage_gateway.download_object.side_effect = Exception("Download failed")
+
+    with pytest.raises(Exception) as exc_info:
+        use_case.execute(dto)
+        
+    assert str(exc_info.value) == "Download failed"
+
+
+@patch('src.infrastructure.repositories.mongoengine.video_job_repository.MongoVideoJobRepository')
+@patch('src.infrastructure.video.ffmpeg_wrapper.FFmpegWrapper')
+@patch('src.infrastructure.gateways.s3_storage_gateway.S3StorageGateway')
+def test_raise_error_when_upload_frames_in_bulk_fails(mock_storage_gateway, mock_ffmpeg_wrapper, mock_video_job_repository):
+    video_job = VideoJobFactory()
+    mock_video_job_repository.find_by_job_ref.return_value = video_job.to_entity()
+    
+    dto = ProcessVideoTaskDTO(
+        job_ref=video_job.job_ref,
+        client_identification=video_job.client_identification,
+        bucket=video_job.bucket,
+        video_path=video_job.video_path,
+        frames_path=video_job.frames_path,
+        notify_url=video_job.notify_url,
+        config=video_job.config,
+    )
+
+    use_case = ProcessVideoUseCase(
+        video_job_repository=mock_video_job_repository,
+        storage_gateway=mock_storage_gateway,
+        video_processor=mock_ffmpeg_wrapper
+    )
+    
+    mock_storage_gateway.download_object.return_value = b"video content"
+    mock_ffmpeg_wrapper.extract_frames.return_value = ["frame1.jpg", "frame2.jpg"]
+    
+    use_case._upload_frames_in_bulk = MagicMock(side_effect=Exception("Upload failed"))
+    
+    with pytest.raises(Exception) as exc_info:
+        use_case.execute(dto)
+        
+    assert str(exc_info.value) == "Upload failed"
